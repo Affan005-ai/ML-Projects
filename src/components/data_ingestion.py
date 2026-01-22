2bd7143af0c877e429a88255063f01875be65954
Affan Nadeem <affannadeem005@gmail.com>
2026-01-22 18:13:13 +0500

    Add data transformation module and update data ingestion process
    - Introduced a new `data_transformation.py` module to handle data preprocessing.
    - Implemented a `Data_Transformation` class with methods for creating a preprocessing pipeline and transforming training and testing data.
    - Updated `data_ingestion.py` to integrate data transformation after ingestion.
    - Added `dill` to requirements for object serialization.
    - Created a new binary file `preprocessor.pkl` to store the preprocessing object.
    - Added a utility function `save_object` for saving objects to disk.

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from src.logger import logging
 from src.exception import CustomException 
 from dataclasses import dataclass
+from src.components.data_transformation import Data_Transformation
+from src.components.data_transformation import data_transformation_config
 
 
 @dataclass
@@ -50,8 +52,10 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data ,test_data=obj.initiate_data_ingestion()
 
+    data_transformation= Data_Transformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
 
             
             
